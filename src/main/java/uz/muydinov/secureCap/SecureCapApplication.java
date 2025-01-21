f0502fa2ec3ec5970d010dd2f389e969e65088a5
JohnUfo <tursunov.umidjon.uz@gmail.com>
2025-01-21 18:49:04 +0500

    first commit

---
+++ src/main/java/uz/muydinov/secureCap/SecureCapApplication.java
@@ -0,0 +1,13 @@
+package uz.muydinov.secureCap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureCapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureCapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/muydinov/secureCap/SecureCapApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.muydinov.secureCap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureCapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
